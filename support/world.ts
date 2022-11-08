5f6e17ce8ab8145fda76875f914bf3f6a37e9b46
Abhi <abhishek.kulkarni@whiparound.com>
2022-11-08 16:34:21 +1300

    Adding config for mobile devices

--- support/world.ts
+++ support/world.ts
@@ -1,5 +1,5 @@
 import { Before, After, setDefaultTimeout } from "@cucumber/cucumber";
-import { Browser, chromium, Page } from "playwright";
+import { Browser, chromium, devices, Page } from "playwright";
 
 let page: Page;
 let browser: Browser;
@@ -9,7 +9,10 @@ setDefaultTimeout(60000);
 Before(async () => {
   try {
     browser = await chromium.launch({ headless: false });
-    const context = await browser.newContext();
+    const context = await browser.newContext({
+      ...devices["Moto G4"],
+      ...devices["iPhone 12"]
+    });
     page = await context.newPage();
   } catch (error) {
     console.log("Error, failed to launch browser");
